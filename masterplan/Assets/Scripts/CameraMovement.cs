e3f635a10a3adb27e4100eb7f81e27dad2fc874c
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-09 12:26:34 +0200

    Insertet Player movement

---
+++ masterplan/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerbody;
+    private float xAxisClamp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    Cursor.lockState = CursorLockMode.Locked;
+        xAxisClamp = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        //Beschränkung des winkels
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+         
+        }
+        else if(xAxisClamp < -90 )
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+            
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerbody.Rotate(Vector3.up * mouseX);
+    }
+    //Magische formel aus dem internet
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ masterplan/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //Variablen
+    public float speed;
+    public float gravity;
+    public float jumpForce;
+    [SerializeField] private float currentGravity = 0;
+    private CharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 FinalMovement = (Movement() * speed) + Gravitation();
+        _characterController.Move(FinalMovement);
+
+        //Gravitation
+        Vector3 Gravitation()
+        {
+            Vector3 gravitymovement = new Vector3(0, -currentGravity, 0);
+            currentGravity += gravity * Time.deltaTime;
+            if (_characterController.isGrounded)
+            {
+                currentGravity = 0.01f;
+            }
+            if (Input.GetButtonDown("Jump") && _characterController.isGrounded)
+            {
+                currentGravity -= jumpForce * Time.deltaTime;
+            }
+            return gravitymovement;
+        }
+
+        //Movement
+        Vector3 Movement()
+        {
+            Vector3 MoveVector = Vector3.zero;
+
+            MoveVector += transform.forward * Input.GetAxis("Vertical");
+            MoveVector += transform.right * Input.GetAxis("Horizontal");
+            MoveVector *= Time.deltaTime;
+            return MoveVector;
+        }
+
+    }
+}
