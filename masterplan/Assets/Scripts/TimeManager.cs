d6e3e3da5a7d7a98af9051e9ea4de40151a748e4
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-12 12:35:07 +0200

    Added BulletTime

---
+++ masterplan/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowDownFactor = 0.05f;
+    public float slowDownLength = 2f;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            DoSlowMotion();
+        }
+            Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+    void DoSlowMotion()
+    {
+        Time.timeScale = slowDownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.2f;
+    }
+
+}
