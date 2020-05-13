f24b4263c98da3311488674e5e5ba43fdbb17900
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-05-13 14:12:33 +0200

    added healthbar

---
+++ masterplan/Assets/Scripts/EnemieManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieManager : MonoBehaviour
+{
+    public HealthBar healthBar;
+    private float health;
+    // Start is called before the first frame update
+    void start()
+
+    {
+        health = GameObject.Find("Enemie").GetComponent<Target>().health; ;
+        Debug.Log(health);
+        healthBar.SetMaxHealth((int)health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.SetHealth((int)health);
+    }
+}
--- masterplan/Assets/Scripts/Target.cs
+++ masterplan/Assets/Scripts/Target.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    
     public void TakeDamage (float amount)
     {
         health -= amount;
-        if (health <= 0f)
+        
+
+        if (health <= 0)
         {
             Die();
         }
