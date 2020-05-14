091ababf8f78864289b22183d2fa0180149a13e6
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-14 12:56:10 +0200

    Added new ability (push force)

---
+++ masterplan/Assets/Scripts/PushForce.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushForce : MonoBehaviour
+{
+    public float force = 1000f;
+    public float radius = 700f;
+    public float damage = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            AbilityForce();
+        }
+    }
+    void AbilityForce()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearByoObject in colliders)
+        {
+            Rigidbody rb = nearByoObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+            Target target = nearByoObject.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
