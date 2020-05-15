ea5c972e98c42f5a2d8a3e788bdc5c5e6629c3e0
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-05-15 10:37:57 +0200

    added health and healthbar

---
+++ masterplan/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public HealthBar healthBar;
+    [SerializeField] private float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.SetMaxHealth((int)health);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.SetHealth((int)health);
+    }
+}
