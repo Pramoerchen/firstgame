e95a964118f535edd705ce091f24cf5fba94b1ae
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-15 13:19:05 +0200

    suizidBomber

--- masterplan/Assets/Scripts/EnemyAI.cs
+++ masterplan/Assets/Scripts/EnemyAI.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public int ExplosionDmg = 50;
     Transform playerTransform;
     UnityEngine.AI.NavMeshAgent myNavMesh;
     public float checkRate = 0.1f;
     float nextCheck;
+    public float radius = 5;
+    bool hasExploded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +26,32 @@ public class EnemyAI : MonoBehaviour
         if (Time.time > nextCheck)
             nextCheck = Time.time + checkRate;
         FollowPlayer();
+        ExplodeAtPlayer();
     }
 
     void FollowPlayer()
     {
         myNavMesh.transform.LookAt(playerTransform);
         myNavMesh.destination = playerTransform.position;
+    }
+    void ExplodeAtPlayer()
+    {
+        //OverlapSphere erstellen um nach Player zu suchen
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var nearByoObject in colliders)
+        {
+           var player =  nearByoObject.GetComponent<PlayerManager>();
+            //Gegner Jagt sich in die Luft sobald Spieler nah genug dran ist und andere objekte in reichweite auch
+            if ( player != null && !hasExploded)
+            {
+                player.GetComponent<PlayerManager>().health -= ExplosionDmg;
+                hasExploded = true;
+            }
+            var enemie = nearByoObject.GetComponent<Target>();
+            if(hasExploded)
+            {
+                enemie.GetComponent<Target>().health -= ExplosionDmg;
+            }
+        }   
     }
 }
