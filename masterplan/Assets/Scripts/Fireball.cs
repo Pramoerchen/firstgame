e285f8257e1261871c352c58ef1b7c9b364c4fd6
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-17 13:11:16 +0200

    Creat a Enemy who shoot Fireballs at the Player

---
+++ masterplan/Assets/Scripts/Fireball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    PlayerManager myPlayerManager;
+    Rigidbody rb;
+    public float speed = 15;
+    public float Damage = 75;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward*speed;
+        Destroy(gameObject, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            var player = other.GetComponent<PlayerManager>();
+            player.changeHealth(-Damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ masterplan/Assets/Scripts/RangeEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeEnemy : MonoBehaviour
+{
+    public GameObject ObjectToShoot;
+    public float FireRate = 1f;
+    UnityEngine.AI.NavMeshAgent myNavMesh;
+    public float checkRate = 0.1f;
+    float nextCheck;
+    Transform playerTransform;
+    private float nextTimeToFire = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myNavMesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        myNavMesh.transform.LookAt(playerTransform);
+        if (Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / FireRate;
+            Instantiate(ObjectToShoot, transform.position,transform.rotation);
+        }
+    }
+}
