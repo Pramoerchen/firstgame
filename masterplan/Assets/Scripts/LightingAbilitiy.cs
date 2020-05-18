8b7e6458361ad185d03e92aa4cfc3562f8d8c1a8
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-18 10:35:14 +0200

    Blitzfähigkeit hinzugefügt (taste 1)

---
+++ masterplan/Assets/Scripts/LightingAbilitiy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingAbilitiy : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public float spawnDistance = 10;
+    public float Energy = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = transform.position;
+        Vector3 playerDirection = transform.forward;
+        Quaternion playerRotation = transform.rotation;
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Instantiate(objectToSpawn, spawnPos, playerRotation);
+        }
+
+    }
+}
---
+++ masterplan/Assets/Scripts/Thunder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour
+{
+    public float range = 10;
+    public float Damage = 5;
+    public float duration = 5;
+    float timescale = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, duration);
+        if(Time.time >= timescale)
+        {
+            timescale = Time.time + 1f;
+            Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+            foreach (Collider nearByoObject in colliders)
+            {
+                Target target = nearByoObject.GetComponent<Target>();
+                if (target != null)
+                     {
+                     target.TakeDamage(Damage);
+                     }
+            }
+        }
+    }
+}
