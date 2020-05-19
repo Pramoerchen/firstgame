e3f72d2cb442fd64ccd9e325d43582b3ad8c33e7
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-19 18:25:03 +0200

    Currentwave imply

--- masterplan/Assets/Scripts/EnemySpawnerScript.cs
+++ masterplan/Assets/Scripts/EnemySpawnerScript.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawnerScript : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
     public float SpawnAmount = 1;
     public float spawnTime = 1f;
     public float spawnDelay = 10f;
+    public int currentWave = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +25,7 @@ public class EnemySpawner : MonoBehaviour
             i++;
         }while (i <= SpawnAmount);
         SpawnAmount *= 2;
+        currentWave++;
+        Debug.Log(currentWave);
     }
 }
---
+++ masterplan/Assets/Scripts/UiCurrentWave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiCurrentWave : MonoBehaviour
+{
+    Text mytext;
+    EnemySpawnerScript myEnemySpawner;
+    public int wave;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mytext = GetComponent<Text>();
+        myEnemySpawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawnerScript>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wave = myEnemySpawner.currentWave;
+        mytext.text = $"Wave: {wave}";
+    }
+}
