0d8a26e3ec8d8d20a6891b311446697f30488111
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-22 19:21:33 +0200

    small changes

--- masterplan/Assets/Scripts/CameraShake.cs
+++ masterplan/Assets/Scripts/CameraShake.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    public IEnumerator Shake (float duration, float magnitude)
-    {
-        Vector3 orginialPos = transform.localPosition;
-        float elapsed = 0.0f;
-        while(elapsed < duration)
-        {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
+    // Set up data
+    bool shake = false;
+    float duration;
+    float magnitude;
+    Vector3 originalPos;
+    float elapsed;
 
-            transform.localPosition = new Vector3(x, y, orginialPos.z);
-            elapsed += Time.deltaTime;
-            yield return null;
+    // Shake the screen
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        // Setup data for camera to be shaked
+        shake = true;
+        this.duration = duration;
+        this.magnitude = magnitude;
+        yield return 0;
+    }
+
+    void Update()
+    {
+        // Shake the camera
+        if (shake)
+        {
+            if (elapsed == 0.0f)
+            {
+                Vector3 originalPos = transform.localPosition; // Save the original position
+            }
+            // Every frame, offset the camera's x and y position for duration seconds for duration seconds
+            if (elapsed < duration)
+            {
+                float x = Random.Range(-1, 1f) * magnitude;
+                float y = Random.Range(-1, 1f) * magnitude;
+                transform.localPosition = new Vector3(x, y, originalPos.z);
+                elapsed += Time.deltaTime;
+            }
+            else // Reset data
+            {
+                transform.localPosition = Vector3.zero;
+                shake = false;
+                elapsed = 0.0f;
+            }
         }
-        transform.localPosition = orginialPos;
     }
 }
