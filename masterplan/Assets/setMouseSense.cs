79c62eab1f9972d04b6d2dca5963d6c2cb98294d
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-05-24 17:14:15 +0200

    Update setMouseSense.cs

--- masterplan/Assets/setMouseSense.cs
+++ masterplan/Assets/setMouseSense.cs
@@ -6,7 +6,16 @@ using UnityEngine.UI;
 public class setMouseSense : MonoBehaviour
 {
     public Slider slider;
-    
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey("mousesense"))
+        {
+            PlayerPrefs.SetFloat("mousesense", 100);
+            PlayerPrefs.Save();
+        }
+        
+    }
+
     void Start()
     {
         slider.value = PlayerPrefs.GetFloat("mousesense");
