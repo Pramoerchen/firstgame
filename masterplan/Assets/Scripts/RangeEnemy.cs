02391834ffada6dcfc585540e6f616f2ec4f8d46
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-05-25 13:25:16 +0200

    new range enemy AI

--- masterplan/Assets/Scripts/RangeEnemy.cs
+++ masterplan/Assets/Scripts/RangeEnemy.cs
@@ -11,6 +11,8 @@ public class RangeEnemy : MonoBehaviour
     float nextCheck;
     Transform playerTransform;
     private float nextTimeToFire = 0f;
+    public float range = 100f;
+    public float FollowRange = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +25,33 @@ public class RangeEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        myNavMesh.transform.LookAt(playerTransform);
-        if (Time.time >= nextTimeToFire)
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
         {
-            nextTimeToFire = Time.time + 1f / FireRate;
-            Instantiate(ObjectToShoot, transform.position,transform.rotation);
+
+            Debug.Log("Raycast ist da");
+            if (hit.transform.tag == "Player")
+            {
+                Debug.Log("Spieler wurde gefunden");
+                if (Time.time >= nextTimeToFire)
+                {
+                    nextTimeToFire = Time.time + 1f / FireRate;
+                    Instantiate(ObjectToShoot, transform.position + new Vector3(0,1,0), transform.rotation);
+                }
+            }
+
+            myNavMesh.transform.LookAt(playerTransform);
+            myNavMesh.destination = playerTransform.position;
+
+        }
+        var distance = (transform.position - playerTransform.position).magnitude;
+        if (distance < FollowRange )
+        {
+            myNavMesh.speed = 0;
+        }else
+        {
+            myNavMesh.speed = 3.5f;
         }
     }
 }
+
