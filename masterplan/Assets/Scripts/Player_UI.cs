0d474d705b4bd2bb6c931aaccc2102693b1f006e
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-05-26 21:26:15 +0200

    create player ui script

--- masterplan/Assets/Scripts/Player_UI.cs
+++ masterplan/Assets/Scripts/Player_UI.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Player_UI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
+
+
 }
