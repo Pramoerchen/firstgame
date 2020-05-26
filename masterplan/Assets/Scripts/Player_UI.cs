09d60a6223cbdc0680dcd0865f749b8ccd511922
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-05-26 22:48:20 +0200

    add ui

--- masterplan/Assets/Scripts/Player_UI.cs
+++ masterplan/Assets/Scripts/Player_UI.cs
@@ -4,14 +4,66 @@ using UnityEngine;
 
 public class Player_UI : MonoBehaviour
 {
-    
+    GameObject ability_1;
+    GameObject ability_2;
+    GameObject ability_3;
+    GameObject ability_4;
+
+    PlayerManager myPlayerManager;
+
+    void Awake()
+    {
+        ability_1 = GameObject.Find("ability1");
+        ability_2 = GameObject.Find("ability2");
+        ability_3 = GameObject.Find("ability3");
+        ability_4 = GameObject.Find("ability4");
+        myPlayerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+
+    }
 
 
+  
+    void Update()
+    {
+
+        if (myPlayerManager.ability_push_isActive)
+        {
+            ability_1.SetActive(true);
+        }
+        else
+        {
+            ability_1.SetActive(false);
+        }
+
+        if (myPlayerManager.ability_barrel_isActive)
+        {
+            ability_2.SetActive(true);
+        }
+        else
+        {
+            ability_2.SetActive(false);
+        }
+
+        if (myPlayerManager.ability_thunder_isActive)
+        {
+            ability_3.SetActive(true);
+        }
+        else
+        {
+            ability_3.SetActive(false);
+        }
+
+        if (myPlayerManager.ability_slowmo_isActive)
+        {
+            ability_4.SetActive(true);
+        }
+        else
+        {
+            ability_4.SetActive(false);
+        }
 
 
-
-
-
+    }
 
 
 }
