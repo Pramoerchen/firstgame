d5a779166ef1003aea25738dcec36a035ef83a55
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-06-02 13:36:56 +0200

    Update GameMaster_Controll.cs

--- masterplan/Assets/Scripts/GameMaster_Controll.cs
+++ masterplan/Assets/Scripts/GameMaster_Controll.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameMaster_Controll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameMaster_Controll Instance;
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // GLOBAL VARS
+
+
+    public float health;
+
+
+
 }
