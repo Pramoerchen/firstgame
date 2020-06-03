1c932ec1e3e72db7dac7937c415f599647d4503d
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-06-03 14:23:43 +0200

    hiddenDoor

---
+++ masterplan/Assets/Scripts/HiddenDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenDoor : MonoBehaviour
+{
+    EnemySpawnerScript myEnemySpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.Find("EnemySpawner"))
+        {
+            myEnemySpawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawnerScript>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myEnemySpawner.currentWave % 5 == 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+}
