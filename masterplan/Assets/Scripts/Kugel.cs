1b4ba550df232ac5a16ce53670d6c01c39f6bc32
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2020-06-07 14:50:21 +0200

    added buggy shotgun and flammen

---
+++ masterplan/Assets/Scripts/Kugel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Kugel : MonoBehaviour
+{
+    
+    public float speed = 15;
+    public float Damage = 20;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
+    public void Setup(UnityEngine.Vector3 shootdir, float bulletspeed)
+    {
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(shootdir * bulletspeed, ForceMode.Impulse);
+
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.transform.GetComponent<Target>().TakeDamage(Damage);
+            Destroy(gameObject);
+        }
+        if (other.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- masterplan/Assets/Scripts/shotgun.cs
+++ masterplan/Assets/Scripts/shotgun.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class shotgun : MonoBehaviour
 {
     public int BulletsShot; // Total bullets show per Shot of the gun
-    public float BulletsSpread; // Degrees (0-360) to spread the Bullets
     public GameObject BulletTemplate; // Bullet to fire
 
-    private int pelletFireVel = 40;
-    public int maxSpread = 5;
+    public float pelletFireVel;
 
     public Transform tip;
 
@@ -24,25 +23,16 @@ public class shotgun : MonoBehaviour
 
     public void Fire()
     {
-        float TotalSpread = BulletsSpread / BulletsShot;
         for (int i = 0; i < BulletsShot; i++)
         {
-            // Calculate angle of this bullet
-            float spreadA = TotalSpread * (i + 1);
-            float spreadB = BulletsSpread / 2.0f;
-            float spread = spreadB - spreadA + TotalSpread / 2;
-            float angle = fpsCam.transform.eulerAngles.y;
 
-            // Create rotation of bullet
-            Quaternion rotation = Quaternion.Euler(new Vector3(0, spread + angle, 0));
-
-            // Create bullet
-            GameObject bullet = Instantiate(BulletTemplate, tip.position, rotation);
             
             
-            //Add randomness to every bullet direction
-            Vector3 dir = transform.forward + new Vector3(Random.Range(-maxSpread, maxSpread), Random.Range(-maxSpread, maxSpread), Random.Range(-maxSpread, maxSpread));
-            bullet.GetComponent<Rigidbody>().AddForce(dir * pelletFireVel);
+            GameObject bullet = Instantiate(BulletTemplate, tip.position, Quaternion.identity);
+
+            Vector3 dir = (fpsCam.transform.forward).normalized;
+            bullet.GetComponent<Kugel>().Setup(dir, pelletFireVel);
+            
         }
 
     }
