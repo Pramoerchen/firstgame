42bd51452bf15290fb1f315d2629f60d9a835eeb
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-06-30 13:47:54 +0200

    Spider web (But need new Textures its BS!)

---
+++ masterplan/Assets/Scripts/Spiderweb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiderweb : MonoBehaviour
+{
+    CharacterMovement myCharMovement;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           myCharMovement =  other.GetComponent<CharacterMovement>();
+            myCharMovement.speed = myCharMovement.speed / 2;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myCharMovement = other.GetComponent<CharacterMovement>();
+            myCharMovement.speed = myCharMovement.speed * 2;
+        }
+    }
+}
